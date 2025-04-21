20ae9c41543691ffa3c1beccb49c3020eb4e7c5c
nisa <tangguhchairunnisa04@gmail.com>
2025-04-21 23:57:22 +0700

    first commit

---
+++ mod3_kel60/ListItems.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ListItems.aspx.cs" Inherits="modul3_kel60.ListItems" %>
+
+
+<!DOCTYPE html>                                                                                                                                                                                                                                
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>ID :</td>
+                    <td>
+                        <asp:TextBox ID="txtIdBuku" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Judul :</td>
+                    <td>
+                        <asp:TextBox ID="txtJudul" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Genre :</td>
+                    <td>
+                        <asp:TextBox ID="txtGenre" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Rilis :</td>
+                    <td>
+                        <asp:TextBox ID="txtRilis" runat="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:Button ID="btnAdd" runat="server" Text="ADD"
+                            OnClick="btnAdd_Click" />
+                        <asp:Button ID="btnDelete" runat="server"
+                            Text="DELETE" OnClick="btnDelete_Click" />
+                        <asp:Button ID="btnUpdate" runat="server"
+                            Text="UPDATE" OnClick="btnUpdate_Click" />
+                        <asp:Button ID="btnClear" runat="server"
+                            Text="CLEAR" OnClick="btnClear_Click" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <asp:GridView ID="GridViewJoin" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ mod3_kel60/ListItems.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+namespace modul3_kel60
+{
+    public partial class ListItems : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter sda = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        DataSet ds = new DataSet();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.ConnectionString = "Data Source=DESKTOP-ACA\\SQLEXPRESS ;Initial Catalog = mod3_perc2_kel60; Integrated Security = True";
+            con.Open();
+            if (!Page.IsPostBack)
+            {
+                DataShow();
+            }
+        }
+        public void DataShow()
+        {
+            ds = new DataSet();
+            cmd.CommandText = "Select * from Buku";
+            cmd.Connection = con;
+            sda = new SqlDataAdapter(cmd);
+            sda.Fill(ds);
+            cmd.ExecuteNonQuery();
+            GridViewJoin.DataSource = ds;
+            GridViewJoin.DataBind();
+            con.Close();
+        }
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Insert Into Buku(ID_Buku, Judul, Genre, Rilis)values('" + txtIdBuku.Text + "', '" + txtJudul.Text.ToString() + "', '" + txtGenre.Text.ToString() + "','" + txtRilis.Text + "')";
+            cmd.Connection = con;
+            cmd.ExecuteNonQuery();
+            DataShow();
+        }
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Update Buku set Judul='" + txtJudul.Text.ToString() + "',  Genre = '" + txtGenre.Text.ToString() + "', Rilis = '" + txtRilis.Text + "' WHERE ID_Buku = '" + txtIdBuku.Text + "'";
+            cmd.Connection = con; cmd.ExecuteNonQuery(); DataShow();
+        }
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            dt = new DataTable();
+            cmd.CommandText = "Delete from Buku where ID_Buku='" + txtIdBuku.Text + "'";
+            cmd.Connection = con; cmd.ExecuteNonQuery(); DataShow();
+        }
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            txtGenre.Text = null;
+            txtIdBuku.Text = null;
+            txtJudul.Text = null;
+            txtRilis.Text = null;
+        }
+    }
+}
